cc5c8ac4b5608821458abdea6dc6d0a5e98a9c44
eamon01 <75913022+eamonwong@users.noreply.github.com>
2024-05-14 19:19:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import tkinter as tk
 import requests
 
-API_KEY = "047313308e4ff618959465753ef430ac"
+# Don't share API_KEY
+API_KEY = ""
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
 
 def get_weather():
